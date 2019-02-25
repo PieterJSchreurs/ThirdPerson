d8e43a04fee523b95a74b976aaeaf1723e83cce9
Pieter Schreurs <pieter_j_schreurs@hotmail.com>
2019-02-25 11:21:03 +0100

    Testing purposes ship change.

--- Project_ThirdPerson/mge_v18_student_version/src/ThirdPerson/Scripts/Ship.cpp
+++ Project_ThirdPerson/mge_v18_student_version/src/ThirdPerson/Scripts/Ship.cpp
@@ -3,7 +3,8 @@
 
 Ship::Ship(Node* pStartNode, std::vector<Node*> pAllNodes, const std::string& aName, const glm::vec3& aPosition) : MovingGridObject(pStartNode, pAllNodes, aName, aPosition)
 {
-
+	//Hello this is a ship for testing merging purposes.
+	std::cout << "Hello " << std::endl;
 }
 
 void Ship::update(float pStep) {
